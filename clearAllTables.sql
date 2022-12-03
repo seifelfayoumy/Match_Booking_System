fa5365ea7f2bab44caa9c89dc1474f2bac84b423
Seif Elfayoumy <seifelfayoumy@gmail.com>
2022-12-03 18:36:07 +0200

    clearAllTables

---
+++ clearAllTables.sql
@@ -0,0 +1,10 @@
+CREATE PROCEDURE clearAllTables
+AS
+DELETE FROM Stadium
+DELETE FROM Match
+DELETE FROM StadiumManager
+DELETE FROM SystemAdmin
+DELETE FROM AssociationManager
+DELETE FROM ClubrRpresentitive
+DELETE FROM ClubStadiumRequest
+DELETE FROM Ticket
